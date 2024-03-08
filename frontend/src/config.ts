be8f3a85a58612053baba4fde221cb47c0ce65cd
angusjellis@live.com <angusjellis@live.com>
2024-03-08 20:23:49 +0000

    feat: add boilerplate frontend

---
+++ frontend/src/config.ts
@@ -0,0 +1,3 @@
+export const API_PORT = process.env.REACT_APP_API_PORT || 8000;
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost";
+export const API_BASE_URL = `${API_URL}:${API_PORT}/api/`;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,26 @@
+import React, {lazy} from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+import './index.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const Home = lazy(() => import('./pages/HomePage'));
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
